784ba30f173f711d3d1a90dd6284693b5544b73c
“[firstname <clarklas831@gmail.com>
2023-11-09 22:19:54 -0600

    office if added and api error handling

--- schedule.py
+++ schedule.py
@@ -2,8 +2,12 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime, timedelta
 import re
-import os
 import json
+import pandas as pd
+import openpyxl
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import time
 
 # Load credentials from a JSON file
 with open('config.json') as config_file:
@@ -23,7 +27,6 @@ worksheet = spreadsheet.sheet1
 
 # Get the value of cell A1
 sunday_value = worksheet.acell('A1').value
-
 
 # Get the current year
 current_year = datetime.now().year
@@ -55,80 +58,105 @@ class Store_Run:
 
 
 def process_employee(employee_name, column_number):
-    # Convert employee_name to uppercase
-    # employee_name = employee_name.upper()
+    # # Convert employee_name to uppercase
+    # employee_name = employee_name.upper
 
-    # Find all occurrences of employee_name in column 2
-    cell_list = worksheet.findall(employee_name, in_column=column_number)
+    try:
 
-    # Iterate through each occurrence
-    for cell in cell_list:
-        # Initialize variables for each iteration
-        store_link = None
-        start_time = None
+        # Find all occurrences of employee_name in column 2
+        cell_list = worksheet.findall(employee_name, in_column=column_number)
 
-        # Step 2: Move up one cell at a time until a link is found
-        current_cell = cell
+        # Iterate through each occurrence
+        for cell in cell_list:
+            # Initialize variables for each iteration
+            store_link = None
+            start_time = None
 
-        # Add the any notes next to name
-        note = worksheet.cell(current_cell.row, current_cell.col + 1).value
-        # Remove new line characters
-        if note:
-            note = note.replace("\n", " ")
+            # Step 2: Move up one cell at a time until a link is found
+            current_cell = cell
 
-        while current_cell.row > 1:
-            current_cell = worksheet.cell(current_cell.row - 1, column_number)
+            # Add the any notes next to name
+            note = worksheet.cell(current_cell.row, current_cell.col + 1).value
+            # Remove new line characters
+            if note:
+                note = note.replace("\n", " ")
 
-            # Check if the content of the cell is not None and is a hyperlink
-            if current_cell.value and re.match(r'^https?://', current_cell.value):
-                store_link = current_cell.value.replace("\n", " ")
-                break
-
-        if store_link:
-            # Step 3: Move up once cell above the link and set that as the store_address
-            store_address = worksheet.cell(
-                current_cell.row - 1, column_number).value.replace("\n", " ")
-
-            # Step 4: Move up one cell from the store_address and set that cell as the store_name
-            store_name = worksheet.cell(
-                current_cell.row - 2, column_number).value.replace("\n", " ")
-
-            # Step 5: Move one cell up from the store_name and set that cell as the inv_type
-            inv_type = worksheet.cell(
-                current_cell.row - 3, column_number).value.replace("\n", " ")
-
-            # Step 6: Move up one cell at a time until a cell is found that starts with a time in the format of HH:MM or H:MM
             while current_cell.row > 1:
                 current_cell = worksheet.cell(
                     current_cell.row - 1, column_number)
-                cell_value = current_cell.value
-                # Check for HH:MM format
-                if cell_value and re.match(r'\d{1,2}:\d{2}', cell_value.strip()):
-                    start_time = cell_value.strip()
+                print(current_cell.value)
+                # Check if the content of the cell is not None and contains the word "OFFICE"
+                if current_cell.value and "OFFICE" in current_cell.value.upper():
+                    store_link = current_cell.value.replace("\n", " ")
+                    store_name = current_cell.value.replace("\n", " ")
+                    store_address = "None"
+                    inv_type = "None"
+                    start_time = "None"
+                    meet_time = "None"
                     break
-                elif cell_value and re.match(r'^https?://', cell_value):
-                    # If another link is found before a time is found, add that link to store_link and repeat steps 3-5
-                    store_link = cell_value.replace("\n", " ")
-                    store_address = worksheet.cell(
-                        current_cell.row - 1, column_number).value.replace("\n", " ")
-                    store_name = worksheet.cell(
-                        current_cell.row - 2, column_number).value.replace("\n", " ")
-                    inv_type = worksheet.cell(
-                        current_cell.row - 3, column_number).value.replace("\n", " ")
+                # Check if the content of the cell is not None and is a hyperlink
+                elif current_cell.value and re.match(r'^https?://', current_cell.value):
+                    store_link = current_cell.value.replace("\n", " ")
+                    break
 
-        # Step 7: Move up one cell from the start_time and set it to the meet_time
-        if start_time:
-            meet_time = worksheet.cell(
-                current_cell.row - 1, column_number).value
+            if store_link and "OFFICE" not in store_link.upper():
+                # Step 3: Move up once cell above the link and set that as the store_address
+                store_address = worksheet.cell(
+                    current_cell.row - 1, column_number).value.replace("\n", " ")
+
+                # Step 4: Move up one cell from the store_address and set that cell as the store_name
+                store_name = worksheet.cell(
+                    current_cell.row - 2, column_number).value.replace("\n", " ")
+
+                # Step 5: Move one cell up from the store_name and set that cell as the inv_type
+                inv_type = worksheet.cell(
+                    current_cell.row - 3, column_number).value.replace("\n", " ")
+
+                # Step 6: Move up one cell at a time until a cell is found that starts with a time in the format of HH:MM or H:MM
+                while current_cell.row > 1:
+                    current_cell = worksheet.cell(
+                        current_cell.row - 1, column_number)
+                    cell_value = current_cell.value
+                    # Check for HH:MM format
+                    if cell_value and re.match(r'\d{1,2}:\d{2}', cell_value.strip()):
+                        start_time = cell_value.strip()
+                        break
+                    elif cell_value and re.match(r'^https?://', cell_value):
+                        # If another link is found before a time is found, add that link to store_link and repeat steps 3-5
+                        store_link = cell_value.replace("\n", " ")
+                        store_address = worksheet.cell(
+                            current_cell.row - 1, column_number).value.replace("\n", " ")
+                        store_name = worksheet.cell(
+                            current_cell.row - 2, column_number).value.replace("\n", " ")
+                        inv_type = worksheet.cell(
+                            current_cell.row - 3, column_number).value.replace("\n", " ")
+
+            # Step 7: Move up one cell from the start_time and set it to the meet_time
+            if start_time:
+                meet_time = worksheet.cell(
+                    current_cell.row - 1, column_number).value
+            else:
+                meet_time = None
+
+            # Create an instance of the Store_Run class
+            store_run_instance = Store_Run(
+                meet_time, start_time, inv_type, store_name, store_address, store_link, note)
+            print(store_run_instance.__dict__)
+
+    except gspread.exceptions.APIError as api_error:
+        if api_error.response.status_code == 429:  # Rate limit exceeded
+            print(f"Rate limit exceeded. Waiting and retrying...")
+            time.sleep(60)  # Sleep for 1 minute (adjust as needed)
+            # Retry the operation
+            process_employee(employee_name, column_number)
         else:
-            meet_time = None
+            # Handle other API errors
+            print(f"API Error: {api_error}")
+    # except Exception as e:
+    #     # Handle other exceptions
+    #     print(f"An unexpected error occurred: {e}")
 
-        # Create an instance of the Store_Run class
-        store_run_instance = Store_Run(
-            meet_time, start_time, inv_type, store_name, store_address, store_link, note)
-        print(store_run_instance.__dict__)
 
-
-columns_to_process = [2, 6, 10, 14, 18, 22, 26]
+columns_to_process = [6]
 for column in columns_to_process:
     process_employee('Lashaun', column)
