dfdd486a9fea726b81eedc4d4aa489cc42efaa29
“[firstname <clarklas831@gmail.com>
2023-11-12 15:58:06 -0600

    styling adjusted

--- script.js
+++ script.js
@@ -33,38 +33,45 @@ fetch('schedule_data.json')
           container.appendChild(item);
         };
 
+        const displayAllStoresButton = (run) => {
+          if (run.store_name.length > 1) {
+            const toggleButton = document.createElement('button');
+            toggleButton.textContent = 'Show All Stores';
+            toggleButton.addEventListener('click', () => {
+              mainCard.innerHTML = ''; // Clear the card before displaying
+
+              if (showAll) {
+                displayItem('Meet Time', run.meet_time, mainCard);
+                displayItem('Start Time', run.start_time, mainCard);
+                displayItem('Note', run.note, mainCard);
+                showAll = false;
+                toggleButton.textContent = 'Show All Stores';
+              } else {
+                displayItem('Meet Time', run.meet_time, mainCard);
+                displayItem('Start Time', run.start_time, mainCard);
+                displayItem('Note', run.note, mainCard);
+                displayAllStores(run, mainCard);
+                showAll = true;
+                toggleButton.textContent = 'Show Less';
+              }
+              mainCard.appendChild(toggleButton); // Ensure the button remains in the card
+            });
+
+            mainCard.appendChild(toggleButton);
+          } else {
+            displayAllStores(run, mainCard);
+          }
+        };
+
         displayItem('Meet Time', run.meet_time, mainCard);
         displayItem('Start Time', run.start_time, mainCard);
         displayItem('Note', run.note, mainCard);
-        displayItem('Store Name', run.store_name[0], mainCard);
-        displayItem('Store Address', run.store_address[0], mainCard);
-        displayItem('Inventory Type', run.inv_type[0], mainCard);
-        displayItem('Store Link', `<a href="${run.store_link[0]}">${run.store_link[0]}</a>`, mainCard);
-
-        dayElement.appendChild(mainCard);
 
         let showAll = false; // Flag to toggle between displaying all stores and just the first
 
-        const toggleButton = document.createElement('button');
-        toggleButton.textContent = 'Show All Stores';
-        toggleButton.addEventListener('click', () => {
-          mainCard.innerHTML = ''; // Clear the card before displaying
+        displayAllStoresButton(run);
 
-          if (showAll) {
-            displayItem('Meet Time', run.meet_time, mainCard);
-            displayItem('Start Time', run.start_time, mainCard);
-            displayItem('Note', run.note, mainCard);
-            showAll = false;
-            toggleButton.textContent = 'Show All Stores';
-          } else {
-            displayAllStores(run, mainCard);
-            showAll = true;
-            toggleButton.textContent = 'Show Less';
-          }
-          mainCard.appendChild(toggleButton); // Ensure the button remains in the card
-        });
-
-        mainCard.appendChild(toggleButton);
+        dayElement.appendChild(mainCard);
       });
 
       scheduleData.appendChild(dayElement);
